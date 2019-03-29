e7741d8256456d84188e6c2d8631f8aa328c8d6a
Andrii Fesenko <oxygendesire@gmail.com>
2019-03-29 14:33:00 +0200

    Homework-7 first commit

---
+++ homework-7/app.js
@@ -0,0 +1,35 @@
+const userName = prompt('Write your name');
+document.getElementById('firsth1').innerHTML+=userName;
+
+
+
+let counter = 0;
+
+function fun1() {
+    let userNumber = prompt('Type a number from 0 to 100');
+    
+    if (!validateUserNumber(userNumber)) {
+        for (i=0; i<userNumber; i++) {
+            counter++;
+            creatingNewEl();
+        }
+    } else {
+        alert('you wrote wrong type. Please type a number from 0 to 100');
+        fun1();
+    }
+}
+
+function creatingNewEl() {
+    const newElem = document.createElement('li');
+    document.body.appendChild(newElem);
+    newElem.textContent = counter;
+}
+
+function validateUserNumber(value){
+    return  (isNaN(value) 
+            || value == ' ' 
+            || value == null
+            || value == '' );
+}
+
+fun1();
