44c59c02016c485af9b49dbdf5462364d2b01654
Andrii Fesenko <oxygendesire@gmail.com>
2019-04-07 02:27:15 +0300

    new commit

--- homework-11/app.js
+++ homework-11/app.js
@@ -2,60 +2,57 @@
 function Student(name, ...marks) {
     this.name = name;
     this.marks = marks;
-    this.averageMark = function getAvg(avg) {
-        let total = 0;
-        for (let i=0; i<marks.length; i++){
-            total += marks[i];
-        }
-        avg = total/marks.length;
-        return avg.toFixed(1);
-        
-    }
-    this.worksDone = function getWorksDone() {
-        let works = 0;
-        for (let i=0; i<marks.length; i++){
-            if (marks[i] > 0) {
-                 works++;
-            }
-        }
-        return works;
-    }
-    this.addMark = function (el) {
-        return marks.push(el);
-    }
+    this.averageMark = averageMark;
+    this.worksDone = worksDone;
+    this.addMark = addMark;
 }
+
+function averageMark() {
+    let arr = this.marks.reduce(function(total, current){
+        return total + current;
+    }, 0)
+    return arr/this.marks.length;
+}
+
+function worksDone() {
+    let works = 0;
+    this.marks.forEach(function(item){
+        if (item > 0) {
+            works++;
+        }
+    }) 
+    return works;
+}
+
+function addMark(el) {
+    return this.marks.push(el);
+}
+
+
+
 
 const students = [
-    new Student('Andrii', 10, 3, 4, 0, 9),
-    new Student('Yulian', 6, 0, 8, 10, 0),
-    new Student('Roman', 10, 8, 10, 10, 0)
+    new Student('Andrii', 10, 3, 2, 0, 9),
+    new Student('Andrii', 0, 3, 4, 0, 2)
 ]
 
-function completePercent(percent) {
-    let total = 0;
-    for (let i=0; i<students.length; i++){
-       let a = students[i].marks
-       console.log(a)
-       for (let i=0; i<a.length; i++) { 
-        if (a[i] > 0 ) {
-            total += 1;
-            }   
-       } 
-       percent = (total*100)/a.length;
-    }
-    return +(percent/students.length).toFixed(1);
+
+
+function groupAverageMark() {
+    let averageAllStudMarks = students.reduce(function(total, current){
+        return total + current.averageMark()
+    }, 0)
+    return averageAllStudMarks/students.length;
 }
 
-function averageMark(avg) {
-    let total = 0;
-    for (let i=0; i<students.length; i++){
-        let a = students[i].marks;
-        for(let i=0; i<a.length; i++) {
-            total += a[i];
-        }
-        avg = total / a.length;
-    }
-    return +avg.toFixed(1);
+function completePercent(){
+    let totalWorks = students.reduce(function(total, current){
+        return total + current.worksDone()
+    }, 0)
+    let allWorks = students.reduce(function(total, current){
+        return total + current.marks.length
+    }, 0)
+    return totalWorks/allWorks*100 + '%';
 }
 
 
