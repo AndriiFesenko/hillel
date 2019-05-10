b96e5081541cd65b4bcace738afe4b32a6952b31
Andrii Fesenko <oxygendesire@gmail.com>
2019-05-10 13:08:32 +0300

    homework-19

--- homework-19/app.js
+++ homework-19/app.js
@@ -1,76 +1,116 @@
-// 'use strict'
-class UserList{
-    static CONTACTS_URL = 'http://fep-app.herokuapp.com/api/contacts';
+'use strict'
+    const CONTACTS_URL = 'http://fep-app.herokuapp.com/api/contacts';
 
-    constructor () {
-        this.addNewUser = document.getElementById('addNewUser');
-        this.userTemplate = document.getElementById('userTemplate').innerHTML;
-        this.tBody = taskList.getElementsByTagName('tbody')[0];
-        this.name = document.getElementById('name');
-        this.surname = document.getElementById('surname');
-        this.email = document.getElementById('email');
-        this.phone = document.getElementById('phone');
-        this.init();
+    const taskList = document.getElementById('taskList');
+    const addNewUserForm = document.getElementById('addNewUserForm');
+    const userTemplate = document.getElementById('userTemplate').innerHTML;
+    const tBody = taskList.getElementsByTagName('tbody')[0];
+    const contactName = document.getElementById('name');
+    const contactSurname = document.getElementById('surname');
+    const contactEmail = document.getElementById('email');
+    const contactPhone = document.getElementById('phone');
+
+    let users = [];
+
+    init();
+    
+    function init() {
+        getUserList(CONTACTS_URL);
+        addNewUserForm.addEventListener('submit', submitForm);
+        tBody.addEventListener('click', onClick);
     }
-
-    init() {
-        this.getUserList();
-        this.addNewUser.addEventListener('submit', () => this.addUser());
+    
+    function onClick(e){
+        if(e.target.parentNode.parentNode == tBody) {
+            changeActiveColor(e);
+        } else if (e.target.value == 'delete'){
+            deletUser(e);
+        }
     }
-
-
-    getUserList(){
-        fetch(UserList.CONTACTS_URL, {
-            method:'get'
-        }).then((resp) => {
-            return resp.json();
-        }).then((user) => this.fetchUsers(user))
+    function changeActiveColor(e){
+        const id = e.target.parentNode.dataset.userId;
+        users.forEach((current) => {
+            if (current.id == id){
+                if(current.is_active){
+                    current.is_active = false
+                } else {
+                    current.is_active = true
+                }
+                changeActiveClass(CONTACTS_URL + '/' + id, current)
+                .then(() => getUserList(CONTACTS_URL))
+            }
+        })
     }
-
-    fetchUsers(user) {
-        this.user = user;
+    function changeActiveClass(url, element){
+        return fetch(url, {
+            method: 'PUT',
+            body: JSON.stringify(element),
+            headers: {
+                'Content-Type': 'application/json; charset=utf-8',
+                'Access-Control-Allow-Origin': '*'
+            }
+        })
+    }
+    function getUserList(url){
+        return fetch(url, {method:'get'})
+                .then((resp) => resp.json())
+                .then(setUsers)
+                .then(renderUsers)
+    }
+    function setUsers(data){
+        users = data;
+        return users;
+    }
+    function renderUsers(user) {
         const userList = user.map((current) => {
-            return this.userTemplate
+            return userTemplate
                         .replace('{{id}}', current.id)
                         .replace('{{name}}', current.name)
                         .replace('{{surname}}', current.surname)
                         .replace('{{email}}', current.email)
                         .replace('{{phone}}', current.phone)
-                        .replace('{{class}}', current === true ? true : false)
+                        .replace('{{class}}', current.is_active ? true : false)
         }).join('\n')
-        this.tBody.innerHTML = userList;
+        tBody.innerHTML = userList;
     }
-   
-    addUser() {
-        const index = this.tBody.lastElementChild.dataset.userId;
+    function submitForm(event){
+        event.preventDefault();
+        addUser();
+        resetForm();
+   }
+   function addUser() {
         const newUser = {
-            name: 0,
-            surname: 0
-            // id: index,
-            // name: this.name.value,
-            // surname: this.surname.value,
-            // email: this.email.value,
-            // phone: this.phone.value
+            name: contactName.value,
+            surname: contactSurname.value,
+            email: contactEmail.value,
+            phone: contactPhone.value,
+            is_active: true
         };
-            this.sendUser(newUser)
-            // .then(this.getUserList)     
+            addUserOnServer(CONTACTS_URL, newUser)
+            .then(() => getUserList(CONTACTS_URL))
     }
-
-    sendUser(newUser){
-        console.log(newUser)
-        fetch(UserList.CONTACTS_URL, {
+    function addUserOnServer(url, element){
+        return fetch(url, {
             method: 'POST',
-            body: JSON.stringify(newUser),
-            headers: {'Content-Type': 'application/json; charset=utf-8',
-                      'Access-Control-Allow-Origin': '*' } 
+            body: JSON.stringify(element),
+            headers: {
+                'Content-Type': 'application/json; charset=utf-8',
+                'Access-Control-Allow-Origin': '*' 
+            }
         })
     }
 
+    function resetForm(){
+        addNewUserForm.reset();
+    }
 
+    function deletUser(e) {
+        const id = e.target.parentNode.parentNode.dataset.userId;
+        const url = CONTACTS_URL + '/' + id
+        deleteUser(url)
+        .then(() => getUserList(CONTACTS_URL))
+    }
 
-
-} // close class
-
-
-
-const userList = new UserList ();
+    function deleteUser(url){
+        return fetch(url, {method: 'DELETE'})
+    }
