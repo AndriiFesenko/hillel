3c7eb4dc226049b4d62a08fa538093d1b7355324
Andrii Fesenko <oxygendesire@gmail.com>
2019-05-25 20:41:04 +0300

    homework-23

---
+++ homework-23/calculator/index.js
@@ -0,0 +1,12 @@
+
+const add = require('./add').add;
+const div = require('./div').div;
+const mult = require('./mult').mult;
+const sub = require('./sub').sub;
+
+module.exports = {
+    add: add,
+    div: div,
+    mult: mult,
+    sub: sub
+}
